6830c074b73b73a85a6b100ff02e7e4607171318
Trivika Komatireddy <komatt@rpi.edu>
2026-01-28 21:25:21 -0500

    Update sample.java

--- sample.java
+++ sample.java
@@ -3,5 +3,7 @@ public class MyClass
     public static void main(String [] args)
     {
         System.out.println("Hello World!");
+        System.out.println("Bye World!");
+        System.out.println("I wonder what will happen when I try to push.");
     }
 }
